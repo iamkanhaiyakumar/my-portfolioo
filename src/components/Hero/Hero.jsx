6664ee92d1be2aaa2278aa9d4725f533efa6a9de
Kanhaiya Kumar <120328606+iamkanhaiyakumar@users.noreply.github.com>
2024-10-02 20:48:42 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -26,7 +26,7 @@ export const Hero = () => {
         <a href="mailto:kanhaiyak0104@gmail.com" className={styles.contactBtn}>
           Contact Me
         </a>
-        <a href="https://drive.google.com/file/d/1AbyabhDMapac6A-tq9Cs8MDGFZT4TyNc/view?usp=sharing" target="blank" className={styles.resumeBtn}>
+        <a href="https://pdf.ac/29zc4s" target="blank" className={styles.resumeBtn}>
           My Resume
         </a>
         </p>
